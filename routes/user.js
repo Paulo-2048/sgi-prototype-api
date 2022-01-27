b92cb07d082edbd959acdb62294b28bbc0abfa43
Paulo Vitor <paulo19032004@gmail.com>
2022-01-27 17:50:18 -0300

    Prettir Code

--- routes/user.js
+++ routes/user.js
@@ -1,76 +1,89 @@
-require('dotenv/config')
-const express = require('express')
+require("dotenv/config")
+const express = require("express")
 const router = express.Router()
-const mysql = require('../mysql').pool
+const mysql = require("../mysql").pool
 
-
-router.get('/', (req, res) => {
-    mysql.getConnection((err, con) => {
-        if(err) {return res.status(500).send({err: err})} 
-        con.query(
-            'SELECT * FROM user',
-            (err, result) => {
-                con.release()
-                if(err) {return res.status(500).send({err: err})} 
-                return res.status(200).send({data: result})
-            }
-        )
-    })
-})
-
-
-router.post('/', (req, res) => {
-    let user = {
-        name: req.body.name,
-        sector: req.body.sector,
-        cpf: req.body.cpf,
-        phone: req.body.phone,
-        email: req.body.email,
-        password: req.body.password,
-        acess: req.body.acess,
-        token: req.body.token,
+router.get("/", (req, res) => {
+  mysql.getConnection((err, con) => {
+    if (err) {
+      return res.status(500).send({ err: err })
     }
+    con.query("SELECT * FROM user", (err, result) => {
+      con.release()
 
-    mysql.getConnection((err, con) => {
-        if(err){
-            console.log('Err Connection:', err)
-        } else {
-            con.query(
-            'INSERT INTO user (name, sector, cpf, phone, email, password, acess, token) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-            [user.name, user.sector, user.cpf, user.phone, user.email, user.password, user.acess, user.token],
-            (err, result, field) => {
-                con.release()
-
-                if (err) {
-                    return res.status(500).send({
-                        error: err,
-                        response: null
-                    })
-                }
-
-                res.status(201).send({
-                    data: 'User insert OK',
-                    idUser: result.idcode
-                })
-            }
-        )}
+      if (err) {
+        return res.status(500).send({ err: err })
+      }
+      return res.status(200).send({ data: result })
     })
+  })
 })
 
+router.post("/", (req, res) => {
+  let user = {
+    name: req.body.name,
+    sector: req.body.sector,
+    cpf: req.body.cpf,
+    phone: req.body.phone,
+    email: req.body.email,
+    password: req.body.password,
+    acess: req.body.acess,
+    token: req.body.token,
+  }
 
-router.get('/:id', (req, res) => {
-    mysql.getConnection((err, con) => {
-        if(err) {return res.status(500).send({err: err})} 
-        con.query(
-            'SELECT * FROM user where idcode = ?',
-            [req.params.id],
-            (err, result) => {
-                con.release()
-                if(err) {return res.status(500).send({err: err})} 
-                return res.status(200).send({data: result})
-            }
-        )
-    })
+  mysql.getConnection((err, con) => {
+    if (err) {
+      console.log("Err Connection:", err)
+    } else {
+      con.query(
+        "INSERT INTO user (name, sector, cpf, phone, email, password, acess, token) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+        [
+          user.name,
+          user.sector,
+          user.cpf,
+          user.phone,
+          user.email,
+          user.password,
+          user.acess,
+          user.token,
+        ],
+        (err, result, field) => {
+          con.release()
+
+          if (err) {
+            return res.status(500).send({
+              error: err,
+              response: null,
+            })
+          }
+
+          res.status(201).send({
+            data: "User insert OK",
+            idUser: result.idcode,
+          })
+        }
+      )
+    }
+  })
+})
+
+router.get("/:id", (req, res) => {
+  mysql.getConnection((err, con) => {
+    if (err) {
+      return res.status(500).send({ err: err })
+    }
+    con.query(
+      "SELECT * FROM user where idcode = ?",
+      [req.params.id],
+      (err, result) => {
+        con.release()
+        if (err) {
+          return res.status(500).send({ err: err })
+        }
+        return res.status(200).send({ data: result })
+      }
+    )
+  })
 })
 
 module.exports = router
