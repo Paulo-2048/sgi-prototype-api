b97c276d1deeab9ffdbaa7400fcd617d5b1125e0
Paulo Vitor <paulo19032004@gmail.com>
2022-01-26 22:29:41 -0300

    Test

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const stripe = require('stripe')(process.env.STRIPE_SK)
 const routUser = require('./routes/user')
 // Other routs down
 
+app.use(express.json({ extended: false }))
 app.use('/user', routUser);
 
 app.get('/', (req, res) => {
