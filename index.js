e187dd5d0660db498fde42c4260e84fb1b7dbdf9
Paulo Vitor <paulo19032004@gmail.com>
2022-02-02 17:20:47 -0300

    Add cors in index.js

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-require('dotenv/config')
-const express = require('express')
+require("dotenv/config")
+const express = require("express")
 const app = express()
-const stripe = require('stripe')(process.env.STRIPE_SK)
+const cors = require("cors")
+const stripe = require("stripe")(process.env.STRIPE_SK)
 
-const routUser = require('./routes/user')
+const routUser = require("./routes/user")
 // Other routs down
 
 app.use(express.json({ extended: false }))
+app.use(cors())
 
-app.use('/user', routUser);
+app.use("/user", routUser)
 
-app.get('/', (req, res) => {
-    res.status(201).send({
-        data: 'Get OK'
-    })
+app.get("/", (req, res) => {
+  res.status(201).send({
+    data: "Get OK",
+  })
 })
 
 const PORT = process.env.PORT || 8080
